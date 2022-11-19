0b2ece27b3f0f8c1e105e2f2092e19c171b1f3e7
kumar1796 <38468059+kumar1796@users.noreply.github.com>
2022-11-19 11:36:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let header = document.getElementById("header");
+console.log(header);
+
+function red() {
+    header.style.color = "red";
+}
+
+function blue() {
+    header.style.color = "blue";
+}
+
+function violet() {
+    header.style.color = "violet";
+}
+
+function indigo() {
+    header.style.color = "indigo";
+}
+
+function border1() {
+    header.style.border = "1px solid Black";
+}
+
+function border2() {
+    header.style.border = "2px solid Black";
+}
+
+function border3() {
+    header.style.border = "3px solid Black";
+}
+
+
+function border4() {
+    header.style.border = "4px solid Black";
+}
+
+function noBorder() {
+    header.style.border = "";
+}
